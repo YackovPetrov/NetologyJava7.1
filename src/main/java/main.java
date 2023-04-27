5033318e7428c5b100ee202f114df61588e15c8e
Yackov <yackov.irkyt@gmail.com>
2023-04-27 14:20:27 +0500

    first method commit

---
+++ src/main/java/main.java
@@ -0,0 +1,20 @@
+public class main {
+    public static void main(String[] args){
+        long[] sales = new long[12];
+        sales[0] = 8;
+        sales[1] = 15;
+        sales[2] = 13;
+        sales[3] = 15;
+        sales[4] = 17;
+        sales[5] = 20;
+        sales[6] = 19;
+        sales[7] = 20;
+        sales[8] = 7;
+        sales[9] = 14;
+        sales[10] = 14;
+        sales[11] = 18;
+        StatsService service = new StatsService();
+        System.out.println(service.calculate(sales));
+
+    }
+}
